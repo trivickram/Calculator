502293bc871d56171bcf9a4b69e168a3df35152f
Trivickram <143883198+trivickram@users.noreply.github.com>
2024-03-12 18:40:04 +0530

    Update Password_Gen.py

--- Password_Gen.py
+++ Password_Gen.py
@@ -1,36 +1,25 @@
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9','!', '#', '$', '%', '&', '*', '+']
-print("Welcome to the password generator")
-length=int(input("Enter the length the password to be generated :"))
-password=""
-for i in range(length):
-    code=random.choice(letters)
-    password+=code
-print(password)
+import string
 
-'''import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-num_l=int(input("Enter the number of letters to included in the password :"))
-num_n=int(input("Enter the number of numbers to included in the password :"))
-num_s=int(input("Enter the number of numbers to included in the password :"))
-password_list=[]
-for i in range(num_l):
-    code=random.choice(letters)
-    password_list.append(code)
+def generate_strong_password():
+    
+    length = int(input("Enter the desired length of the password: "))
+    strength = input("Enter the desired strength of the password (weak, medium, strong): ").lower()
 
-for i in range(num_n):
-    code = random.choice(numbers)
-    password_list.append(code)
+    if strength == 'weak':
 
-for i in range(num_s):
-    code=random.choice(symbols)
-    password_list.append(code)
-random.shuffle(password_list)
+        all_characters = string.ascii_letters
+    elif strength == 'medium':
 
-password=""
-for i in password_list:
-    password=password+i
-print(password)
-'''
+        all_characters = string.ascii_letters + string.digits
+    elif strength == 'strong':
+   
+        all_characters = string.ascii_letters + string.digits + string.punctuation
+    else:
+        print("Invalid strength input. Please enter 'weak', 'medium', or 'strong'.")
+        return
+
+
+    password = ''.join(random.choice(all_characters) for _ in range(length))
+    return password
+print(f"Generated Password: {generate_strong_password()}")
